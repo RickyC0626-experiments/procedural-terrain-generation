e2683b4b0a8a4c5aa343e5af516af4495f32d8b4
Ricky Chon <rickychon99@gmail.com>
2020-01-02 02:55:47 -0500

    Upload assets

---
+++ 01. Noise Map/Procedural Terrain Generation/Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        // Create a two-dimensional array within the bounds of the map
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        // Prevent division by 0
+        if(scale <= 0) scale = 0.0001f;
+
+        // For each point on the map, assign a perlin noise value between 0 and 1
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+        return noiseMap;
+    }
+}
