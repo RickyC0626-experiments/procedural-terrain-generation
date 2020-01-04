c7089a7d8a867fe9d9b133a9e9de02121fe80968
Ricky Chon <rickychon99@gmail.com>
2020-01-04 00:11:27 -0500

    Add assets from Level of Detail update

---
+++ 05. Level of Detail/Procedural Terrain Generation/Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    // Renderer of the plane we want to display our map on
+    public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    public void DrawTexture(Texture2D texture)
+    {
+        // Apply the texture to the texture renderer
+        // Use sharedMaterial instead of material to generate texture within the editor, since material is instantiated at runtime
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        // Use sharedMesh and sharedMaterial to generate mesh within the editor
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+    }
+}
---
+++ 05. Level of Detail/Procedural Terrain Generation/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode { NoiseMap, ColorMap, Mesh };
+    public DrawMode drawMode;
+
+    const int mapChunkSize = 241;
+    [Range(0, 6)]
+    public int levelOfDetail;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistence;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        // Generate the noise map with the necessary parameters
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistence, lacunarity, offset);
+
+        // Create an array to hold all the colors on the map
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+
+        // Iterate through each coordinate on the noiseMap
+        for(int y = 0; y < mapChunkSize; y++)
+        {
+            for(int x = 0; x < mapChunkSize; x++)
+            {
+                // Get the height value at the coordinates
+                float currentHeight = noiseMap[x, y];
+
+                // Iterate through the regions to find which color corresponds to the height
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        // Converting the index of two-dimensional array to one-dimensional
+                        // y * width will give us the row, and adding x will give us the column
+                        
+                        // Assign the region color associated with the height
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        
+        if(drawMode == DrawMode.NoiseMap) display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        else if(drawMode == DrawMode.ColorMap) display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
+        else if(drawMode == DrawMode.Mesh) display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
+    }
+
+    // Called whenever a public variable is modified in the inspector
+    void OnValidate()
+    {
+        if(octaves < 0) octaves = 0;
+        if(lacunarity < 1) lacunarity = 1;
+    }
+}
+
+// Make serializable to show in inspector
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
