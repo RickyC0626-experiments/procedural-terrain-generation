fd2bb3abb4dae89173237af0bb14bbdaa30534d8
Ricky Chon <rickychon99@gmail.com>
2020-01-04 22:16:32 -0500

    Add assets from Endless Terrain update

---
+++ 06. Endless Terrain/Procedural Terrain Generation/Assets/Scripts/InfiniteTerrain.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 450;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int visibleChunksInViewDist;
+
+    // Coordinates, corresponding terrain chunk
+    Dictionary<Vector2, TerrainChunk> terrainChunkDict = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+    void Start()
+    {
+        // Dimensions of the actual mesh will be 1 less than mapChunkSize (241)
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        visibleChunksInViewDist = Mathf.RoundToInt(maxViewDistance / chunkSize);
+    }
+
+    void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks()
+    {
+        // Clear chunks from last update to prevent chunks beyond maxViewDistance to remain loaded
+        for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++)
+        {
+            terrainChunksVisibleLastUpdate[i].SetVisible(false);
+        }
+        terrainChunksVisibleLastUpdate.Clear();
+
+        // Get the coordinate of the chunk the viewer is standing on
+        // Current chunk coord (0, 0)
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        // 240 vertices to 1 chunk coord
+        // Chunk to the right of current chunk: (240, 0) --> (1, 0)
+        for(int yOffset = -visibleChunksInViewDist; yOffset <= visibleChunksInViewDist; yOffset++)
+        {
+            for (int xOffset = -visibleChunksInViewDist; xOffset <= visibleChunksInViewDist; xOffset++)
+            {
+                // Only instantiate a new terrain chunk at this coordinate if one is not created already
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                // Maintain a dictionary of all the coordinates and terrain chunks to prevent duplicates
+                if(terrainChunkDict.ContainsKey(viewedChunkCoord))
+                {
+                    terrainChunkDict[viewedChunkCoord].UpdateTerrainChunk();
+
+                    if(terrainChunkDict[viewedChunkCoord].IsVisible())
+                    {
+                        terrainChunksVisibleLastUpdate.Add(terrainChunkDict[viewedChunkCoord]);
+                    }
+                }
+                else terrainChunkDict.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, transform));
+            }
+        }
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        {
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionVector3 = new Vector3(position.x, 0, position.y);
+
+            // Instantiate a plane as visual placeholder
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionVector3;
+            meshObject.transform.localScale = (Vector3.one * size) / 10f;
+            meshObject.transform.parent = parent;
+            SetVisible(false);
+        }
+
+        // Find the point on the chunk's perimeter that is closest to the viewer's position and find the distance between that point and the viewer
+        // If the distance is less than maxViewDistance, then meshObject is enabled
+        // If the distance exceeds maxViewDistance, then meshObject is disabled
+        public void UpdateTerrainChunk()
+        {
+            float viewerDistFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            bool visible = viewerDistFromNearestEdge <= maxViewDistance;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
---
+++ 06. Endless Terrain/Procedural Terrain Generation/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGenerator
+{
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
+    {
+        // Get the length of the first dimension array 
+        int width = heightMap.GetLength(0);
+        // Get the length of the second dimension array 
+        int height = heightMap.GetLength(1);
+
+        /*
+            x   x   x
+            -1  0   1
+
+            Left most point: x = (w - 1) / -2 = -1
+            Top most point: z = (h - 1) / 2 = 1
+
+            z 1
+            z 0
+            z -1
+        */
+        float topLeftX = (width - 1) / -2f;
+        float topLeftZ = (height - 1) / 2f;
+
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        /*
+            w = 9
+            i = 1   0   1   2   3   4   5   6   7   8       (9 - 1) / 1 + 1 = 8 + 1 = 9 vertices
+            i = 2   0       2       4       6       8       (9 - 1) / 2 + 1 = 4 + 1 = 5 vertices
+            i = 4   0               4               8       (9 - 1) / 4 + 1 = 2 + 1 = 3 vertices
+
+            i = factor of (w - 1)
+            i = 1, 2, 4, 8
+
+            (w - 1) / i + 1
+        */
+        int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
+
+        // Create a new meshData for the heightMap
+        MeshData meshData = new MeshData(width, height);
+
+        int vertexIndex = 0;
+
+        // Iterate through each coordinate on the heightMap based on level of detail
+        for(int y = 0; y < height; y += meshSimplificationIncrement)
+        {
+            for (int x = 0; x < width; x += meshSimplificationIncrement)
+            {
+                // Assign the height value to each vertex starting from the top left corner
+                // topLeftX + x --> move rightward
+                // topLeftZ - y --> move downward
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
+                // Let each vertex know where the uv is in relation to the rest of the map as a percentage
+                meshData.uvs[vertexIndex] = new Vector2(x / (float) width, y / (float) height);
+
+                // Right and bottom edges of the map do not have triangles to set
+                if(x < width - 1 && y < height - 1)
+                {
+                    /*
+                        i       i + 1 ...
+
+                        i + w   i + w + 1 ...
+
+                        Triangle 1: i,          i + w + 1,  i + w
+                        Triangle 2: i + w + 1,  i,          i + 1
+                    */
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
+                }
+                vertexIndex++;
+            }
+        }
+        // Return the meshData instead of mesh to allow implementation of threading so that the game doesn't freeze
+        // when different chunks of the mesh are generated
+        return meshData;
+    }
+}
+
+// A mesh consists of triangles arranged in a 3D space to create the impression of a solid object
+public class MeshData
+{
+    // Contains the positions of vertices
+    public Vector3[] vertices;
+    // Contains indices to the vertex array
+    public int[] triangles;
+    // UV: texture coordinates of the mesh
+    public Vector2[] uvs;
+
+    int triangleIndex;
+
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
+        // (width - 1)(height - 1) gives number of squares
+        // Each square consists of 2 triangles with 3 vertices each
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+        triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        // Fix the lighting for a smoother transition
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+}
